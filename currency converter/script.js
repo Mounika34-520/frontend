e9043d7ad55e86413b408567078c19605042b3ad
Mounika34-520 <kummaraguntamounika@gmail.com>
2023-09-18 12:14:47 +0530

    commit

--- currency converter/script.js
+++ currency converter/script.js
@@ -1,37 +1,32 @@
-var input_amount = document.getElementById("original-currency-amount");
-var from_currency = document.getElementById("from_currency");
-var to_currency = document.getElementById("to_currency");
-var exchange_rate = document.getElementById("exchange-rate");
-var exchange = document.getElementById("exchange");
-var output_amount = document.getElementById("output-text");
-var output_from = document.getElementById("from");
-var output_to = document.getElementById("to");
+var select = document.querySelectorAll(".currency"),
+input_currency = document.getElementById('input_currency'),
+output_currency = document.getElementById('output_currency');
 
-exchange.addEventListener("click", () => {
-  [from_currency.value, to_currency.value] = [
-    to_currency.value,
-    from_currency.value,
-  ];
-  calculate();
+fetch(`https://api.frankfurter.app/currencies`)
+  .then((data) => data.json())
+  .then((data) => {
+    const entries = Object.entries(data);
+    console.log(data)
+	  for (var i = 0; i < entries.length; i++) {
+	    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+	    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+	  }
+   	
 });
 
-var to_amount = 0;
-function calculate() {
-  const from_currency_value = from_currency.value;
-  const to_currency_value = to_currency.value;
-
-  fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency_value}`)
-    .then((res) => res.json())
-    .then((res) => {
-      const rate = res.rates[to_currency_value];
-      exchange_rate.value = `${rate}`;
-      to_amount = (input_amount.value * rate).toFixed(3);
-      output_from.innerText = `${input_amount.value} ${from_currency_value}`;
-      output_to.innerText = `${to_amount} ${to_currency_value}`;
-      output_amount.style.display = "block";
-    });
+function convert(){
+ 	input_currency_val = input_currency.value;
+ 	if(select[0].value != select[1].value ){
+ 		alert("yes")
+ 		const host = 'api.frankfurter.app';
+		fetch(`https://${host}/latest?amount=${input_currency_val}&from=${select[0].value}&to=${select[1].value}`)
+		  .then((val) => val.json())
+    	.then((val) => {
+		    //alert(`10 GBP = ${data.rates.USD} USD`);
+		    output_currency.value = Object.values(val.rates)[0]
+		    console.log(Object.values(val.rates)[0])
+		});
+ 	}else{
+ 		alert("Peease select two different currencies")
+ 	}
 }
-
-document.getElementById("exchange_button").addEventListener("click", () => {
-  calculate();
-});
